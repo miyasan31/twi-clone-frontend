06fa25c429e95737812b19af8fffe1c9499424ab
miyasan_0301 <miyasan.19990301@gmail.com>
2021-09-02 01:41:27 +0900

    🔧  update:stitches v1.0.0

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable import/no-default-export */
 import NextDocument, { Head, Html, Main, NextScript } from "next/document";
-import { getCssString } from "src/utils";
+import { getCssText } from "src/utils";
 
 export default class Document extends NextDocument {
 	render() {
 		return (
 			<Html lang="en">
 				<Head>
-					<style id="stitches" dangerouslySetInnerHTML={{ __html: getCssString() }} />
+					<style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
 				</Head>
 				<body>
 					<Main />
