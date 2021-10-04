ce027e90db5e69c46c6575a7539c20dd0c747f4a
miyasan_0301 <miyasan.19990301@gmail.com>
2021-10-04 12:07:20 +0900

    update:.tsx-to-.page.tsx

---
+++ src/pages/home/[id].page.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+
+const HomeExamplePage: NextPage = () => {
+	return <div>HOME</div>;
+};
+
+// eslint-disable-next-line import/no-default-export
+export default HomeExamplePage;
