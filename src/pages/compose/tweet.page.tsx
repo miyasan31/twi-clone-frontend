50617f2774b00c8d2530f8a22ec1b6951f36de29
miyasan_0301 <miyasan.19990301@gmail.com>
2021-10-05 12:32:41 +0900

    feat:page

---
+++ src/pages/compose/tweet.page.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import { Text } from "src/components/shared";
+
+const TweetCreatePage: NextPage = () => {
+	return (
+		<div>
+			<div>
+				<Text>a</Text>
+			</div>
+		</div>
+	);
+};
+
+// eslint-disable-next-line import/no-default-export
+export default TweetCreatePage;
