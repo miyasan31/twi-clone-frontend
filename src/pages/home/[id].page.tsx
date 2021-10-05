f8815e83c97457c06cd8853962da3931927ff18e
miyasan_0301 <miyasan.19990301@gmail.com>
2021-10-05 12:05:55 +0900

    remove:file

--- src/pages/home/[id].page.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from "next";
-
-const HomeExamplePage: NextPage = () => {
-	return <div>HOME</div>;
-};
-
-// eslint-disable-next-line import/no-default-export
-export default HomeExamplePage;
