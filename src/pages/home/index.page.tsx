8a61bbff5779a827df0683256cc07b650f96ddad
miyasan_0301 <miyasan.19990301@gmail.com>
2021-10-06 20:10:56 +0900

    feat:home-page

--- src/pages/home/index.page.tsx
+++ src/pages/home/index.page.tsx
@@ -1,15 +1,50 @@
 import type { NextPage } from "next";
-import { Text } from "src/components/shared";
+import { Navigator } from "src/components/layout/Navigator";
+import { ThemeChanger } from "src/components/theme";
+import { styled } from "src/utils";
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
 	return (
-		<div>
-			<div>
-				<Text>a</Text>
+		<>
+			<div className="fixed right-0 bottom-3">
+				<ThemeChanger />
 			</div>
-		</div>
+
+			<Layout>
+				<Left>
+					<Navigator />
+				</Left>
+
+				<Main>{props.children}</Main>
+
+				<Right></Right>
+			</Layout>
+		</>
 	);
 };
 
 // eslint-disable-next-line import/no-default-export
 export default HomePage;
+
+const Layout = styled("div", {
+	display: "flex",
+});
+
+const Left = styled("div", {
+	display: "flex",
+	justifyContent: "end",
+	width: "360px",
+	paddingTop: "0.25rem",
+	paddingRight: "2rem",
+});
+
+const Main = styled("div", {
+	height: "100vh",
+	width: "600px",
+	borderRight: "1px solid $slate6",
+	borderLeft: "1px solid $slate6",
+});
+
+const Right = styled("div", {
+	flex: 1,
+});
