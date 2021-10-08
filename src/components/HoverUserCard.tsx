cd4f830bff09017762c19c0c865b8a232d660375
miyasan_0301 <miyasan.19990301@gmail.com>
2021-10-08 16:15:41 +0900

    feat:event.preventDefault

--- src/components/HoverUserCard.tsx
+++ src/components/HoverUserCard.tsx
@@ -1,6 +1,7 @@
 import * as HoverCardPrimitive from "@radix-ui/react-hover-card";
 import Image from "next/image";
-import type { ReactNode, VFC } from "react";
+import type { MouseEvent, ReactNode, VFC } from "react";
+import { useCallback, useMemo } from "react";
 import { Button, Flex, NextLink, Text } from "src/components/shared";
 import { slideDownAndFade, slideLeftAndFade, slideRightAndFade, slideUpAndFade } from "src/components/shared/animation";
 import { CARD_ICON_PHOTO_SIZE } from "src/constants/icon_photo";
@@ -16,21 +17,36 @@ type Props = {
 	followerCount: string;
 };
 
+const useHrefs = (userId: string) => {
+	const profileHref = useMemo(() => `/${userId}`, [userId]);
+	const followerHref = useMemo(() => `/${userId}/followers`, [userId]);
+	const followingHref = useMemo(() => `/${userId}/followings`, [userId]);
+	return { profileHref, followerHref, followingHref };
+};
+
 export const HoverUserCard: VFC<Props> = (props) => {
-	const handleClick = () => {
+	const { profileHref, followerHref, followingHref } = useHrefs(props.userId);
+
+	const handleClickDisabled = useCallback((e: MouseEvent) => {
+		e.preventDefault();
+	}, []);
+
+	const handleClick = useCallback(() => {
 		console.info("click!!");
-	};
+	}, []);
 
 	return (
 		<HoverCardPrimitive.Root>
-			<HoverCardPrimitive.Trigger>{props.children}</HoverCardPrimitive.Trigger>
+			<HoverCardPrimitive.Trigger>
+				<NextLink href={profileHref}>{props.children}</NextLink>
+			</HoverCardPrimitive.Trigger>
 
-			<HoverCardContent sideOffset={5}>
+			<HoverCardContent sideOffset={5} onClick={(e) => handleClickDisabled(e)}>
 				<HoverCardArrow offset={25} />
 
 				<Flex direction="col">
 					<Flex justify="between">
-						<NextLink href="/">
+						<NextLink href={profileHref}>
 							<Image
 								src={props.iconPhoto}
 								alt="Picture of the author"
@@ -46,7 +62,7 @@ export const HoverUserCard: VFC<Props> = (props) => {
 
 					<Flex direction="col" gap={1}>
 						<Text pt={0.5}>
-							<NextLink href="/">
+							<NextLink href={profileHref}>
 								<Text bold under>
 									{props.userName}
 								</Text>
@@ -60,19 +76,22 @@ export const HoverUserCard: VFC<Props> = (props) => {
 						<Text>{props.profileBody}</Text>
 
 						<Flex gap={1}>
-							<NextLink href="/" under>
-								<Flex>
-									<Text bold>{props.followingCount}</Text>
-									<Text faded>&nbsp;Following</Text>
-								</Flex>
-							</NextLink>
+							<TextWrap>
+								<NextLink href={followingHref}>
+									<FollowCount>
+										<CountText>{props.followingCount}</CountText>
+										&nbsp;フォロー
+									</FollowCount>
+								</NextLink>
+							</TextWrap>
 
-							<NextLink href="/" under>
-								<Flex>
-									<Text bold>{props.followerCount}</Text>
-									<Text faded>&nbsp;Followers</Text>
-								</Flex>
-							</NextLink>
+							<TextWrap>
+								<NextLink href={followerHref}>
+									<FollowCount>
+										<CountText>{props.followerCount}</CountText>&nbsp;フォロワー
+									</FollowCount>
+								</NextLink>
+							</TextWrap>
 						</Flex>
 					</Flex>
 				</Flex>
@@ -80,6 +99,21 @@ export const HoverUserCard: VFC<Props> = (props) => {
 		</HoverCardPrimitive.Root>
 	);
 };
+
+const FollowCount = styled("span", {
+	color: "$slate11",
+	fontSize: "0.85rem",
+});
+
+const CountText = styled("span", {
+	color: "$slate12",
+	fontWeight: 700,
+	fontSize: "1rem",
+});
+
+const TextWrap = styled("span", {
+	"&:hover": { textDecoration: "underline" },
+});
 
 export const HoverCardArrow = styled(HoverCardPrimitive.Arrow, {
 	fill: "$slate3",
