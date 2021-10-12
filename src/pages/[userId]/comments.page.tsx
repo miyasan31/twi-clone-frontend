b9ce24085e85bbae00e0a85470c8ad93b3ce6e7a
miyasan_0301 <miyasan.19990301@gmail.com>
2021-10-13 02:37:04 +0900

    feat:pages

--- src/pages/[userId]/comments.page.tsx
+++ src/pages/[userId]/comments.page.tsx
@@ -1,5 +1,19 @@
-const UserCommentsPage = () => {
-	return <div></div>;
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { MainBody } from "src/components/layout/MainBody";
+import { TopContentTitle } from "src/components/layout/TopContentTitle";
+import { ProfileDetailCard } from "src/components/ProfileDetailCard";
+
+const UserCommentsPage: NextPage = () => {
+	const router = useRouter();
+
+	return (
+		<MainBody>
+			<TopContentTitle title={router.query.userId} subtitle="2,987件のツイート" isBrowserBack />
+
+			<ProfileDetailCard />
+		</MainBody>
+	);
 };
 
 // eslint-disable-next-line import/no-default-export
--- src/pages/[userId]/likes.page.tsx
+++ src/pages/[userId]/likes.page.tsx
@@ -1,5 +1,19 @@
-const UserLikesPage = () => {
-	return <div></div>;
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { MainBody } from "src/components/layout/MainBody";
+import { TopContentTitle } from "src/components/layout/TopContentTitle";
+import { ProfileDetailCard } from "src/components/ProfileDetailCard";
+
+const UserLikesPage: NextPage = () => {
+	const router = useRouter();
+
+	return (
+		<MainBody>
+			<TopContentTitle title={router.query.userId} subtitle="2,987件のツイート" isBrowserBack />
+
+			<ProfileDetailCard />
+		</MainBody>
+	);
 };
 
 // eslint-disable-next-line import/no-default-export
