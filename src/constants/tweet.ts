2bb9d91762213641ea21258d4a53a348e1fb940e
miyasan_0301 <miyasan.19990301@gmail.com>
2021-10-13 18:58:16 +0900

    feat:constant-userData

--- src/constants/tweet.ts
+++ src/constants/tweet.ts
@@ -1,6 +1,4 @@
-import type { TweetDetailCardProps } from "src/components/TweetDetailCard";
-
-export const TWEET_DATA: TweetDetailCardProps = {
+export const TWEET_DATA = {
 	id: 1,
 	userId: "miyasan_0301",
 	tweetBody: "ツイートする",
@@ -14,4 +12,18 @@ export const TWEET_DATA: TweetDetailCardProps = {
 	commentCount: { count: "1" },
 	retweetCount: { count: "3" },
 	likeCount: { count: "10" },
+};
+
+export const USER_DATA = {
+	id: "miyasan_0301",
+	userName: "みやさん",
+	profileBody:
+		"JavaScript TypeScriptで書ける技術を中心に学習しています｜アルバイトで新規事業開発のエンジニアとして修行中｜2022.04〜東京でWEBエンジニア",
+	iconPhoto: "",
+	followerCount: {
+		count: "10000",
+	},
+	followingCount: {
+		count: "100",
+	},
 };
