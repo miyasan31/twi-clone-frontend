b09b8408831a07ca50233fcc27e50b11725d5e3a
miyasan_0301 <miyasan.19990301@gmail.com>
2021-10-20 01:20:05 +0900

    update:constant

--- src/constants/tweet.ts
+++ src/constants/tweet.ts
@@ -8,7 +8,10 @@ export const TWEET_DATA = {
 		profileBody:
 			"JavaScript TypeScriptで書ける技術を中心に学習しています｜アルバイトで新規事業開発のエンジニアとして修行中｜2022.04〜東京でWEBエンジニア",
 		iconPhoto: "",
+		isFollow: true,
 	},
+	isLike: true,
+	isRetweet: true,
 	commentCount: { count: "1" },
 	retweetCount: { count: "3" },
 	likeCount: { count: "10" },
